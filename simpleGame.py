88d4e8492b9da0d55e81016e7f6e23eb353ae44b
Kevin Talik <Kevin Talik>
2018-04-30 14:41:23 -0700

    A water Block now spawns randomly on the screen

--- simpleGame.py
+++ simpleGame.py
@@ -1,28 +1,43 @@
-import pygame,sys,random
+import pygame,sys,random, threading,time
 from pygame.locals import *
+from random import randint
+
+
+''' GLOBAL VARIABLES '''
 
 pygame.init()
 FPS=30
 fpsClock=pygame.time.Clock()
 
-DISPLAYSURF=pygame.display.set_mode((720,720))
-pygame.display.set_caption('Fun Block')
+windowSize = 700 #this should be a square window
 
-blockImage = pygame.image.load('smallObject.png')
+DISPLAYSURF=pygame.display.set_mode((windowSize,windowSize))
+pygame.display.set_caption("Don't tip the tank!")
 
+mainFish = pygame.image.load('static/simpleFish.png')
+waterImage = pygame.image.load('static/waterBlock.png')
 
 #randomRectangle = pygame.Rect(10,20,200,300)
 pos = {"x":10,"y":10}
 deltaX = 0
 deltaY = 0
 directionStart = 'right'
+rate = 20
+waterPos = (windowSize/2,windowSize/2)
+totalWaterBlocks=10
 
-rate = 10
+def randBlockPos():
+    global waterPos
+    for b in xrange(totalWaterBlocks):
+        waterPos = (randint(0,windowSize),randint(0,windowSize))
+        time.sleep(3)
+    
+t = threading.Thread(target=randBlockPos)
+t.start()
+
 '''MAIN GAME LOOP'''
 while True:
     DISPLAYSURF.fill((0,0,0))
-
-    
     for event in pygame.event.get():
         
         deltaX = 0
@@ -44,8 +59,10 @@ while True:
 
             pos["x"] = pos["x"] + deltaX
             pos["y"] = pos["y"] + deltaY
+    
         DISPLAYSURF.fill((0,0,0))
-        DISPLAYSURF.blit(blockImage,(pos["x"],pos["y"]))
+        DISPLAYSURF.blit(mainFish,(pos["x"],pos["y"]))
+        DISPLAYSURF.blit(waterImage,waterPos)
         pygame.display.flip()
         pygame.display.update()
         fpsClock.tick(FPS)
