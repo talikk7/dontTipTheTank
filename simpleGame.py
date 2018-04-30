077aced8f0d5151a43ec38b32708c98dc314fbc4
Kevin Talik <Kevin Talik>
2018-04-30 14:52:40 -0700

    added comments so this code is human readable

--- simpleGame.py
+++ simpleGame.py
@@ -11,9 +11,10 @@ fpsClock=pygame.time.Clock()
 
 windowSize = 700 #this should be a square window
 
-DISPLAYSURF=pygame.display.set_mode((windowSize,windowSize))
+DISPLAYSURF=pygame.display.set_mode((windowSize,windowSize)) #see how its a square window
 pygame.display.set_caption("Don't tip the tank!")
 
+#here are some random images
 mainFish = pygame.image.load('static/simpleFish.png')
 waterImage = pygame.image.load('static/waterBlock.png')
 
@@ -25,12 +26,15 @@ directionStart = 'right'
 rate = 20
 waterPos = (windowSize/2,windowSize/2)
 totalWaterBlocks=10
+evaporation=3
 
+''' THREADING THINGS '''
 def randBlockPos():
+    #Updates the water blocks position 
     global waterPos
     for b in xrange(totalWaterBlocks):
         waterPos = (randint(0,windowSize),randint(0,windowSize))
-        time.sleep(3)
+        time.sleep(evaporation)
     
 t = threading.Thread(target=randBlockPos)
 t.start()
@@ -40,8 +44,11 @@ while True:
     DISPLAYSURF.fill((0,0,0))
     for event in pygame.event.get():
         
+        #resets position changes
         deltaX = 0
         deltaY = 0
+        
+        #Keyboard Events
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
@@ -56,10 +63,11 @@ while True:
             if event.key == pygame.K_DOWN:
                 deltaY = (rate)       
         
-
+            #updates the position
             pos["x"] = pos["x"] + deltaX
             pos["y"] = pos["y"] + deltaY
-    
+        
+        #updates the screen
         DISPLAYSURF.fill((0,0,0))
         DISPLAYSURF.blit(mainFish,(pos["x"],pos["y"]))
         DISPLAYSURF.blit(waterImage,waterPos)
