215794d36c8b38f6aeda3f05f106da46366bc660
Kevin Talik <Kevin Talik>
2018-05-09 17:12:21 -0700

    collisions are working but the fish is too small. Going fishing

--- simpleGame.py
+++ simpleGame.py
@@ -6,34 +6,66 @@ from random import randint
 ''' GLOBAL VARIABLES '''
 
 pygame.init()
-FPS=30
+pygame.font.init()
+FPS=15
 fpsClock=pygame.time.Clock()
 
-windowSize = 700 #this should be a square window
+windowSize = 300 #this should be a square window
 
 DISPLAYSURF=pygame.display.set_mode((windowSize,windowSize)) #see how its a square window
 pygame.display.set_caption("Don't tip the tank!")
+theFont = pygame.font.SysFont('Comic Sans MS', 30)
+
+#init the scoreboard
+_SCORE = 0
+scoreBoard = theFont.render("Score: " + str(_SCORE), False, (0,0,0))
+
 
 #here are some random images
 mainFish = pygame.image.load('static/simpleFish.png')
 waterImage = pygame.image.load('static/waterBlock.png')
+_OLIVE = (128,128,0)
 
 #randomRectangle = pygame.Rect(10,20,200,300)
 pos = {"x":10,"y":10}
 deltaX = 0
 deltaY = 0
 directionStart = 'right'
-rate = 20
-waterPos = (windowSize/2,windowSize/2)
+rate = 50
+waterPos = {"x":windowSize/2,"y":windowSize/2}
 totalWaterBlocks=10
 evaporation=3
+
+''' FUNCTIONS FOR FUNCTIONALITY '''
+def incrementScore():
+    global theFont
+    global scoreBoard
+    global _SCORE
+    _SCORE = _SCORE + 1
+    scoreBoard = theFont.render("Score: " + str(_SCORE),False,(0,0,0))
+
+
+def checkCollision(thing1,thing2):
+    if( cmp(thing1,thing2) ):
+        print "THERE IS A COLLISION"
+        print "Thing 1:", thing1
+        print "Thing 2:", thing2
+        return True
+    else:
+        return False
+
+def compareTuple(t1,t2):
+    if(t1["x"] == t2["x"]) and (t2["y"] == t2["y"]):
+        return True
+    else:
+        return False
 
 ''' THREADING THINGS '''
 def randBlockPos():
     #Updates the water blocks position 
     global waterPos
     for b in xrange(totalWaterBlocks):
-        waterPos = (randint(0,windowSize),randint(0,windowSize))
+        waterPos = {"x":randint(0,windowSize),"y":randint(0,windowSize)}
         time.sleep(evaporation)
     
 t = threading.Thread(target=randBlockPos)
@@ -41,7 +73,7 @@ t.start()
 
 '''MAIN GAME LOOP'''
 while True:
-    DISPLAYSURF.fill((0,0,0))
+    DISPLAYSURF.fill(_OLIVE)
     for event in pygame.event.get():
         
         #resets position changes
@@ -67,10 +99,14 @@ while True:
             pos["x"] = pos["x"] + deltaX
             pos["y"] = pos["y"] + deltaY
         
-        #updates the screen
-        DISPLAYSURF.fill((0,0,0))
-        DISPLAYSURF.blit(mainFish,(pos["x"],pos["y"]))
-        DISPLAYSURF.blit(waterImage,waterPos)
-        pygame.display.flip()
-        pygame.display.update()
-        fpsClock.tick(FPS)
+        
+    if compareTuple( waterPos,pos ):
+        incrementScore() #magic number for the score
+    #updates the screen
+    DISPLAYSURF.fill(_OLIVE)
+    DISPLAYSURF.blit(mainFish,(pos["x"],pos["y"]))
+    DISPLAYSURF.blit(waterImage,(waterPos["x"],waterPos["y"]) )
+    DISPLAYSURF.blit(scoreBoard,(0,0))
+    pygame.display.flip()
+    pygame.display.update()
+    fpsClock.tick(FPS)
